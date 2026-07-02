686b0974655f14b7eadb875b2f02d452ad555bbe
Sabrina Kleman <sabrina@wincast.io>
2026-07-02 15:45:43 -0400

    pic

--- app/paintings/planesandtrees/page.tsx
+++ app/paintings/planesandtrees/page.tsx
@@ -11,9 +11,9 @@ export default function PlanesAndTrees() {
       image="/images/planesandtrees.jpeg"
       description={
         <p>This piece asks up front (rudely): Since when did cubism stop being about the cube? How many two dimensional planes does one canvas’s two dimensional plane really need? Is reality itself imposed on a dimensional plane we’re eternally tethered to, forever unable to control the more fundamental forces of the higher dimensional planes which impose their will on us without our consent? This piece takes everything that is funny about that idea and makes it look very cool to your eyes. 
-        </p>}
-     
-}  extraImages={[
+        </p>
+      }
+      extraImages={[
     "/images/planesandtrees2.jpeg",
   ]}/>
   );
