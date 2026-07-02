acab8a094c42b384462c85e586c290c7d16142d8
Sabrina Kleman <sabrina@wincast.io>
2026-07-02 15:48:00 -0400

    fix errors

--- app/paintings/lemon/page.tsx
+++ app/paintings/lemon/page.tsx
@@ -8,7 +8,7 @@ export default function Lemon() {
       medium="Oil on panel"
       dimensions='11" × 14"'
       image="/images/lemon.jpg"
-      statement={
+description={
   <>
     {/* <p>
       Medieval painting (c. 500–1400 AD) evolved from early Christian and Roman styles, prioritizing spiritual symbolism over realism to educate a largely illiterate audience. Dominated by religious narratives, it thrived through illuminated manuscripts, frescos, and panel painting, evolving from stylized Byzantine forms to the more naturalistic, detailed approaches of the late Gothic period.
